2c5a39524de62def113dc40cf0e5ea55449c37c2
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-05-05 21:20:35 -0300

    :construction: Mission Impulse: Stage 4/5

---
+++ mobile/src/components/Copyright/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import { styles } from './styles';
+
+export function Copyright() {
+  return (
+    <View>
+      <Text style={styles.text}>Feito com ♥ pela Rocketseat</Text>
+    </View>
+  );
+}
---
+++ mobile/src/libs/api.ts
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: 'http://152.244.113.72:3333'
+});
